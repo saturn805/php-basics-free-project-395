cd7f78fa515a13173638ccc06ca6ffc30e8797ba
Pirr <133140972+saturn805@users.noreply.github.com>
2026-09-04 07:13:42 +0000

    Перепроверить перед тестами

--- PasswordGenerator.php
+++ PasswordGenerator.php
@@ -7,13 +7,20 @@ const UPPERCASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const DIGITS = '0123456789';
 const SPECIAL = '!@#$%^&*';
 
-// Генерация псевдо-случайного числа
+const MIN_LENGTH = 8;  // For the password length check condition
+
+
+// ================================================
+// Password generation
+// ================================================
+
+// Generation of a pseudo‑random number
 function nextRandom(int $number): int
 {
     return (16807 * $number) % 2147483647;
 }
 
-// Основная функция генерации пароля
+// Main function for password generation
 function generatePassword (
     int $length, 
     int $seed, 
@@ -44,4 +51,114 @@ function generatePassword (
     }
 
     return $result;
+}
+
+
+// ================================================
+// Main func for checking password strength
+// ================================================
+function checkPassword(string $password): string
+{
+    $estimation = 0;
+
+    // Password length check
+    if (isLongPassword($password)) {
+        $estimation += 1;
+    }
+
+    // Check for lowercase letters
+    if (hasLowercase($password)) {
+        $estimation += 1;
+    }
+
+    // Check for uppercase letters
+    if (hasUppercase($password)) {
+        $estimation += 1;
+    }
+
+    // Check for the presence of digits
+    if (hasDigits($password)) {
+        $estimation += 1;
+    }
+
+    // Check for special characters
+    if (hasSpecial($password)) {
+        $estimation += 1;
+    }
+
+    // Reaching a verdict
+    return getVerdict($estimation);
+}
+
+// ================================================
+// Helper functions for checking password
+// ================================================
+
+// Password length check
+function isLongPassword(string $password): bool
+{
+    if (strlen($password) >= MIN_LENGTH) {
+        return true;
+    }
+
+    return false;
+}
+
+// Checking for lowercase letters in the password
+function hasLowercase(string $password): bool
+{
+    foreach (str_split($password) as $char) {
+        if (str_contains(LOWERCASE, $char)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// Checking for uppercase letters in the password
+function hasUppercase(string $password): bool
+{
+    foreach (str_split($password) as $char) {
+        if (str_contains(UPPERCASE, $char)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// Checking for numbers in the password
+function hasDigits(string $password): bool
+{
+    foreach (str_split($password) as $char) {
+        if (str_contains(DIGITS, $char)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// Checking for special characters in the password
+function hasSpecial(string $password): bool
+{
+    foreach (str_split($password) as $char) {
+        if (str_contains(SPECIAL, $char)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// Receiving the verdict
+function getVerdict(int $estimation): string
+{
+    return match (true) {
+        $estimation <= 2 => "Слабый пароль (оценка {$estimation} из 5)",
+        $estimation === 3 => "Средний пароль (оценка {$estimation} из 5)",
+        $estimation === 4 => "Надежный пароль (оценка {$estimation} из 5)",
+        $estimation === 5 => "Очень надежный пароль (оценка {$estimation} из 5)",
+    };
 }
--- main.php
+++ main.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/PasswordGenerator.php';
 
 use function hexlet\code\generatePassword;
+use function hexlet\code\checkPassword;
 
 // Помощник для переноса строки при неоднократном вызове функции
 function println (string $value): void
@@ -15,3 +16,9 @@ println(generatePassword(12, 123));
 println(generatePassword(12, 123, useSpecial: true));
 println(generatePassword(8, 1, useUppercase: false, useDigits: false));
 println(generatePassword(-3, 42));
+
+println(checkPassword('abc'));
+println(checkPassword('abcdefgh'));
+println(checkPassword('abcdef1234'));
+println(checkPassword('Abcdef1234'));
+println(checkPassword('Abcdef123!'));
